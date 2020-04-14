b0f0cdd804d796a14048ba7026c6e97d51706cd8
tboyd4 <tboyd1898@gmail.com>
2020-04-14 10:31:46 -0500

    currently using the fetch hook to bring in pokemon at launch, testing pokeapi

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,33 @@
 <template>
   <div>
-    <h1
-      v-for="(pokemon, index) in pokemons"
-      :key="index"
-    > {{ pokemon.name }} </h1>
+    <button @click="fetchPokemons">Click for Mons</button>
+    <h1 v-for="(pokemon, index) in pokemons" :key="index">{{ pokemon.name }}</h1>
   </div>
 </template>
 
 <script>
-import {mapState} from 'vuex';
+import { mapState, mapActions } from "vuex";
 
 export default {
   head() {
     return {
-      title: 'Pokemon'
-    }
+      title: "Pokemon"
+    };
   },
   async fetch({ store, error }) {
     try {
-      await store.dispatch('pokemons/fetchPokemons')
+      await store.dispatch("pokemons/fetchPokemons");
     } catch (e) {
       error({
         statusCode: 503,
-        message: 'Unable to fetch pokemons at this time. Please try again.'
-      })
+        message: "Unable to fetch pokemons at this time. Please try again."
+      });
     }
   },
-  computed: mapState({
-    pokemons: state => state.pokemons.pokemons
-  })
-  
-}
+  computed: {
+    ...mapState({
+      pokemons: state => state.pokemons.pokemons
+    })
+  }
+};
 </script>
